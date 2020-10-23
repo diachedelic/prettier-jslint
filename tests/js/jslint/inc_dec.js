ae3fa5e3edd8aa5b58905938e7d0ad7d099e5201
James Diacono <james@diacono.com.au>
2020-10-23 18:50:02 +1100

    Increment/decrement test

---
+++ tests/js/jslint/inc_dec.js
@@ -0,0 +1,36 @@
+let a = i++;
+a = i--;
+a = ++i;
+a = --i;
+
+i++;
+++i;
+
+if (true) {
+  i--;
+  --i;
+}
+
+function one() {
+  return --i;
+}
+function two() {
+  return i--;
+}
+function three() {
+  return ++i;
+}
+function found() {
+  return i++;
+}
+
+for (let i = Things.length - 1; i >= 0; i--) {
+  Things[i] += 1;
+  Things[i] -= 1;
+}
+
+stuff (a++);
+
+while(a++) {
+  bar();
+}
